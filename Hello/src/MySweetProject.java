d472bc8a053ee71c5090c23d71faff8cc58e1aef
Chad Hirsch <43390100+DarkSky42@users.noreply.github.com>
2021-02-17 14:22:29 -0500

    Initial Commit

---
+++ Hello/src/MySweetProject.java
@@ -0,0 +1,23 @@
+import java.*;
+import java.util.Scanner;
+
+public class MySweetProject {
+
+	public static void main(String[] args) { //method
+		
+		System.out.println("Enter your name: ");
+		
+		//Scanner in = new Scanner(System.in); 
+		//String s = in.nextLine();
+		
+	
+		int i = 0;
+		do {
+			System.out.println(" i is: " + i);
+			i++;
+		} while(i < 10); 
+		
+		
+		//in.close();
+	} 
+}
