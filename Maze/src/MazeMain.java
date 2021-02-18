5782076db3824965b3fc6b89839552b48e65506a
chad-hirsch <chad.hirsch2@gmail.com>
2021-02-18 13:32:40 -0500

    Maze Movement and backtracking

--- Maze/src/MazeMain.java
+++ Maze/src/MazeMain.java
@@ -5,8 +5,8 @@ public class MazeMain {
 
 	static int[][] maze = {
 			{2, 0, 1, 1}, //start in upper right
-			{1, 1, 0, 1},
-			{0, 1, 1, 1}
+			{1, 1, 1, 0},
+			{0, 0, 0, 1}
 	}; 
 	static //0=wall
 	//1=path
@@ -15,10 +15,50 @@ public class MazeMain {
 	LinkedList<Position> path = new LinkedList<Position>();
 	public static void main(String[] args) {
 		
-		Position p = new Position(3, 0); //start position. 
+		Position p = new Position(0, 3); //start position. 
 		path.push(p); 
 		
-		maze[path.peek().y][path.peek().x] = 0;
+		
+		while(true) {
+			int y = path.peek().y;
+			int x = path.peek().x;
+			
+			//down
+			if(maze[y+1][x] == 2) {
+				System.out.println("Moved Down. You Won!");
+			}else if(maze[y+1][x] == 1) {
+				System.out.println("Moved Down");
+				path.push(new Position(y+1, x) );
+				continue;
+			}
+			
+			//left
+			if(maze[y][x-1] == 2) {
+				System.out.println("Moved Left. You Won!");
+			}else if(maze[y][x-1] == 1) {
+				System.out.println("Moved Left");
+				path.push(new Position(y, x-1) );
+				continue;
+			}
+			
+			//up
+			if(maze[y-1][x] == 2) {
+				System.out.println("Moved Up. You Won!");
+			}else if(maze[y-1][x] == 1) {
+				System.out.println("Moved Up");
+				path.push(new Position(y-1, x) );
+				continue;
+			}
+			
+			//right
+			if(maze[y][x+1] == 2) {
+				System.out.println("Moved Right. You Won!");
+			}else if(maze[y][x+1] == 1) {
+				System.out.println("Moved Right");
+				path.push(new Position(y, x+1) );
+				continue;
+			}
+		}
 		
 	}
 
