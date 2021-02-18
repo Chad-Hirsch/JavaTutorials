dc55af5e5e78895a4f90e31829f7f287bca580d1
chad-hirsch <chad.hirsch2@gmail.com>
2021-02-18 17:53:40 -0500

    Using Hashmaps

--- Collections/src/Collections.java
+++ Collections/src/Collections.java
@@ -1,11 +1,18 @@
+import java.util.HashMap;
+import java.util.Set;
 
 public class Collections {
 
 	public static void main(String[] args) {
-		int[][] ages = new int[5][];
-		ages[0] = new int[10];
-		ages[1] = new int[3]; 
-
+		HashMap<String, Integer> ids = new HashMap<String, Integer>();
+		ids.put("Chad Hirsch", 5);
+		ids.put("Bobby", ids.getOrDefault("Bobby", 0) + 1); //if bobby does not exist I use getOrDefault. 
+		
+		Set<String> keys = ids.keySet(); //This will give all the keys in the hashmap. 
+		for(String key : keys) {
+			System.out.println(key);
+			System.out.println(ids.get(key));
+		}
+		
 	}
-
 }
