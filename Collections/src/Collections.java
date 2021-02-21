67254671c0dbcf20cab0ceaa7060767a8ed68140
chad-hirsch <chad.hirsch2@gmail.com>
2021-02-21 15:32:22 -0500

    Using hashcode to store Strings.

--- Collections/src/Collections.java
+++ Collections/src/Collections.java
@@ -1,18 +1,12 @@
-import java.util.HashMap;
-import java.util.Set;
+
 
 public class Collections {
 
 	public static void main(String[] args) {
-		HashMap<String, Integer> ids = new HashMap<String, Integer>();
-		ids.put("Chad Hirsch", 5);
-		ids.put("Bobby", ids.getOrDefault("Bobby", 0) + 1); //if bobby does not exist I use getOrDefault. 
-		
-		Set<String> keys = ids.keySet(); //This will give all the keys in the hashmap. 
-		for(String key : keys) {
-			System.out.println(key);
-			System.out.println(ids.get(key));
-		}
+		String hello = "Hello";
+		String bye = "bye";
+		System.out.println(hello.equals(bye));
+
 		
 	}
 }
